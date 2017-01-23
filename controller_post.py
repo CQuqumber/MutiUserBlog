2544728afa07b42d4d16d1ad08bc6a22b6d9b8f2
Kueipo <cququmber@gmail.com>
2017-01-23 22:03:34 +0800

    remove class Drop

--- controller_post.py
+++ controller_post.py
@@ -54,29 +54,3 @@ class PostPage(BlogHandler):
 
 
 
-
-class DropPost(BlogHandler):
-    def get(self, post_id):
-
-        key = ndb.Key('Post', int(post_id), parent=blog_key())
-        post = key.get()
-
-        if self.user and self.user.key.id() == post.user_id:
-            key = ndb.Key('Post', int(post_id), parent=blog_key())
-            post = key.get()
-            key.delete()
-
-            self.redirect('/')
-
-        elif not self.user:
-            self.redirect('/login')
-
-        else:
-            error = "This is not your post!"
-            self.render('post.html',post=post, error=error)
-
-
-
-
-
-
